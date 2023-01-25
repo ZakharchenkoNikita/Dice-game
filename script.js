6a214a0aa2656d29326f74ad0d5a1879a9c2d0af
immortalprogrammist <immortalvelial@icloud.com>
2023-01-25 13:05:20 +0100

    bug fixed

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ btnHold.addEventListener("click", function () {
       scores[activePlayer];
 
     // check if player`s score is >= 100
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       // finish the game
       playing = false;
       diceEl.classList.add("hidden");
